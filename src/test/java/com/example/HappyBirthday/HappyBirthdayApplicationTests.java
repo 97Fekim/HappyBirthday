3e34fbad03a90a7b9b0c76c17bed3a86b8b4f89f
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-01-24 17:31:18 +0900

    Initial commit

---
+++ src/test/java/com/example/HappyBirthday/HappyBirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HappyBirthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyBirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
