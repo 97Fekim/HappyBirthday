ae037db205e5aa1986e226665e2956a5c7463601
User-2021SVQTSS\Administrator <16fekim@gmail.com>
2022-01-25 11:02:23 +0900

    create PresentRepsitory

---
+++ src/main/java/com/example/HappyBirthday/repository/MemoryPresentRepository.java
@@ -0,0 +1,12 @@
+package com.example.HappyBirthday.repository;
+
+import com.example.HappyBirthday.repository.domain.Present;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryPresentRepository {
+    private static List<Present> store = new ArrayList<>();
+
+
+}
